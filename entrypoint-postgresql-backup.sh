1b168d6f481176c532ce0109a062c50ffba879f9
James Hodgkinson <james@terminaloutcomes.com>
2024-05-13 10:31:03 +1000

    wrong env var for s3 uploads, reducing container size

--- entrypoint-postgresql-backup.sh
+++ entrypoint-postgresql-backup.sh
@@ -4,7 +4,9 @@ set -e
 
 postgresql-backup.sh
 
-if [ -n "${S3_BUCKET}" ]; then
+if [ -n "${S3_BACKUP_BUCKET_NAME}" ]; then
     s3_backup
+else
+    echo "No S3_BACKUP_BUCKET_NAME specified, skipping upload."
 fi
 
