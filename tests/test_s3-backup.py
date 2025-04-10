7505a0879186b151fb1d71ced2ecbb696e659f58
James Hodgkinson <james@terminaloutcomes.com>
2025-04-10 14:01:19 +1000

    maint: minor fix to catch docker errors in a test

--- tests/test_s3-backup.py
+++ tests/test_s3-backup.py
@@ -2,6 +2,7 @@ import io
 import os
 import tempfile
 from unittest import mock
+import docker
 from pydantic import ValidationError
 import pytest
 from testcontainers.minio import MinioContainer  # type: ignore
@@ -31,33 +32,38 @@ def test_minio_container() -> None:
     )
     with open(tempfilename, "w") as f:
         f.write("hello world\n")
-    with MinioContainer(image="minio/minio:latest") as minio:
-        with mock.patch.dict(
-            os.environ,
-            {
-                f"{ENV_PREFIX}ENDPOINT_URL": f"http://{minio.get_config()['endpoint']}",
-                f"{ENV_PREFIX}FILENAME": tempfilename,
-            },
-            clear=False,
-        ):
-            client = minio.get_client()
-            client.make_bucket(Config().bucket_name)  # type: ignore
-            test_content = b"Hello World"
-            for daynum in range(1, 7):
-                yesterday = datetime.now(UTC) - timedelta(days=daynum)
-                filename = f"backup-testfile-{yesterday.strftime('%Y%m%d-%H%M')}.tar.gz"
-                client.put_object(
-                    Config().bucket_name,  # type: ignore
-                    filename,
-                    io.BytesIO(test_content),
-                    length=len(test_content),
-                    metadata={"LastModified": yesterday.isoformat()},
-                )
-                print(f"Put {filename}")
-            # with NamedTemporaryFile(prefix=f"backup-testfile-{datetime.now(UTC).strftime('%Y%m%d-%H%M')}", suffix=".tar.gz") as tmpfile:
+    try:
+        with MinioContainer(image="minio/minio:latest") as minio:
+            with mock.patch.dict(
+                os.environ,
+                {
+                    f"{ENV_PREFIX}ENDPOINT_URL": f"http://{minio.get_config()['endpoint']}",
+                    f"{ENV_PREFIX}FILENAME": tempfilename,
+                },
+                clear=False,
+            ):
+                client = minio.get_client()
+                client.make_bucket(Config().bucket_name)  # type: ignore
+                test_content = b"Hello World"
+                for daynum in range(1, 7):
+                    yesterday = datetime.now(UTC) - timedelta(days=daynum)
+                    filename = (
+                        f"backup-testfile-{yesterday.strftime('%Y%m%d-%H%M')}.tar.gz"
+                    )
+                    client.put_object(
+                        Config().bucket_name,  # type: ignore
+                        filename,
+                        io.BytesIO(test_content),
+                        length=len(test_content),
+                        metadata={"LastModified": yesterday.isoformat()},
+                    )
+                    print(f"Put {filename}")
+                # with NamedTemporaryFile(prefix=f"backup-testfile-{datetime.now(UTC).strftime('%Y%m%d-%H%M')}", suffix=".tar.gz") as tmpfile:
 
-            res = main(use_file_path=True)
-            assert res == 4
+                res = main(use_file_path=True)
+                assert res == 4
+    except docker.errors.DockerException:
+        pytest.skip("Docker is not running or not available")
 
 
 def test_get_date_from_file_path() -> None:
